d108255867f21b901240636379699ae18b33377c
= <r.gaziano@gmail.com>
2023-12-14 00:07:32 +0100

    lol server code should work better with an actual server...

---
+++ src/server.zig
@@ -0,0 +1,66 @@
+const std = @import("std");
+const os = std.os;
+const net = std.net;
+
+const recv_request = @import("nw.zig").recv_request;
+
+const HOST = "127.0.0.1";
+const PORT = 9999;
+
+pub var gpa = std.heap.GeneralPurposeAllocator(.{
+    // Debug opts
+    .stack_trace_frames = 10,
+    .never_unmap = true,
+    .retain_metadata = true,
+    // .verbose_log = true,
+}){};
+
+pub var arena = std.heap.ArenaAllocator.init(gpa.allocator());
+pub const allocator = arena.allocator();
+
+pub fn main() !void {
+    const server = try os.socket(os.AF.INET, os.SOCK.STREAM, 0);
+    defer os.closeSocket(server);
+
+    const addr = try net.Address.resolveIp(HOST, PORT);
+
+    try os.bind(server, &addr.any, addr.getOsSockLen());
+    try os.listen(server, 1);
+    std.debug.print("Listening on port: {}\n", .{PORT});
+
+    while (true) {
+        const client = try os.accept(server, null, null, 0);
+        defer os.closeSocket(client);
+        std.debug.print("Accepted client connection: XXX\n", .{});
+        // std.debug.print("{}", std.os.getsockname(client, os.AF.INET, null));
+        // std.debug.print("{}", std.os.getpeername(client, os.AF.INET, null));
+
+        var buf = [_]u8{0} ** 256;
+
+        const len = try os.recv(client, buf[0..], 0);
+
+        var reply = std.ArrayList(u8).init(allocator);
+
+        if (len > 0) {
+            std.debug.print("received: {s}\n", .{buf[0..len]});
+            const resp = recv_request(allocator, buf[0..len]);
+
+            std.debug.print("Response length: {}\n", .{resp.len});
+            // std.debug.print("Response: {s}\n", .{resp});
+
+            _ = try reply.writer().write(resp[0..]);
+            _ = try reply.writer().write("\n");
+            _ = try os.send(client, reply.items, 0);
+            // Expicit null byte to tell client to stop listening
+            // _ = try os.send(client, "\x00", 0);
+        }
+
+        if (!arena.reset(.retain_capacity)) {
+            std.log.err("Could not reset temp memory!", .{});
+        }
+    }
+
+    _ = arena.reset(.free_all);
+    _ = gpa.deinit();
+    std.debug.print("shutting down\n", .{});
+}
